c371c5ab8e1812be64a4c907cbf4fcc50b4604ff
Valentina Panic <valentina.panic87@gmail.com>
2020-05-16 18:59:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ end
 end
 
 def all_holidays_with_bbq(holiday_hash)
- holiday_hash.each do |season, holiday|
+         holiday_hash.each do |season, holiday|
          season.each do |holiday, supply|
            if supply == ("BBQ")
               puts "#{holiday}"
