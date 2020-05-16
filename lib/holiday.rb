6f8a8c7ffa073760c30e2a7d12aa69344642aeb2
Valentina Panic <valentina.panic87@gmail.com>
2020-05-16 18:50:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,10 +71,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
  holiday_hash.collect do |season, holiday|
          season.collect do |holiday, supply|
-
            if supply == "BBQ"
               puts ':#{holiday}'
            end
+         end
      end
    end
   # return an array of holiday names (as symbols) where supply lists
