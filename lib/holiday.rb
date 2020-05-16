ea0e783f78ec4afb821ff22526d555584c253d6b
Valentina Panic <valentina.panic87@gmail.com>
2020-05-16 18:43:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,7 +52,7 @@ end
 def all_supplies_in_holidays(holiday_hash)
 holiday_hash.collect do |season,holiday|
   if season == :winter
-#message =  season.to_s.capitalize!+":"
+ puts season.to_s.capitalize!+":"
       holiday.collect do |holiday_name, supplies|
       puts holiday_name.to_s.split("_").join(" ").capitalize + ":" + supplies.join(", ")
 
